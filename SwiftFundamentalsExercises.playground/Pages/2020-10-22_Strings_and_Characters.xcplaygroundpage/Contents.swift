49eb0e145d303ffd0fec2645695999ab3a6f8072
Eric Widjaja <ericwidjaja@pursuit.org>
2020-10-22 14:19:53 -0400

    solution up to question 11

--- SwiftFundamentalsExercises.playground/Pages/2020-10-22_Strings_and_Characters.xcplaygroundpage/Contents.swift
+++ SwiftFundamentalsExercises.playground/Pages/2020-10-22_Strings_and_Characters.xcplaygroundpage/Contents.swift
@@ -118,4 +118,94 @@ func checkPalindrome (in inputStr: String) -> Bool {
     }
     return outputStr == inputStr
 }
-checkPalindrome(in: "racecar")
+//checkPalindrome(in: "racecar")
+
+/*
+ Question 9
+ You are given a string stored in variable problem. Write code so that you print each word of the string on a new line.
+
+ let problem = "split this string into words and print them on separate lines"
+
+ // Your code
+ Example: Input: let problem ="split this string into words and print them on separate lines"
+
+ Output:
+
+ split
+ this
+ string
+ into
+ words
+ and
+ print
+ them
+ on
+ separate
+ lines
+ */
+
+let problem = "split this string into words and print them on separate lines"
+//for word in problem.split(separator: " "){
+//    print("\(word)")
+//}
+func splitStr (inputStr: String, separatorChar: Character) -> String {
+    for word in inputStr.split(separator: separatorChar) {
+        print(word)
+    }
+    return "\(inputStr)"
+}
+//splitStr(inputStr: problem, separatorChar: " ")
+/*
+ 
+ Question 10
+ You are given a string stored in variable problem. Write code that prints the longest word in the string.
+
+ let problem = "find the longest word in the problem description"
+
+ // Your code here
+ Example: Input: let problem = "find the longest word in the problem description"
+ Output: description
+ Hint: Keep track of the longest word you encounter and also keep track of its length.
+ */
+
+let problemLong = "find the longest word in the problem description"
+
+func findLongestWord(in inputStr: String) -> String {
+    
+    var longestWord = ""
+    for word in inputStr.split(separator: " ") {
+        if word.count > longestWord.count {
+            longestWord = String(word)
+        }
+    }
+    return longestWord
+}
+findLongestWord(in: problemLong)
+
+/*
+ Question 11
+ Given a string in English, create a tuple containing the number of vowels and consonants.
+
+ let vowels = "aeiou"
+ let consonants = "bcdfghjklmnpqrstvwxyz"
+ let input = "Count how many vowels I have!"
+ */
+
+func howManyVowelsAndConsonants(in inputStr: String) -> String {
+    var vowelsCount = 0
+    var consonantsCount = 0
+    let vowels = "aeiou"
+    let consonants = "bcdfghjklmnpqrstvwxyz"
+    
+    for char in inputStr {
+        if vowels.contains(char.lowercased()) {
+            vowelsCount += 1
+        } else {
+            if consonants.contains(char.lowercased()) {
+                consonantsCount += 1
+            }
+        }
+    }
+    return "There are \(String(vowelsCount)) vowels and \(String(consonantsCount)) consonants "
+}
+print(howManyVowelsAndConsonants(in: "Pursuit"))
